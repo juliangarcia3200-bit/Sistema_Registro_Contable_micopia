9e42db78feaa026d5821badeb3ba4d3a5611ef6c
juliangarcia3200 <juliangarcia3200@gmail.com>
2025-08-19 17:27:19 -0500

    Agrego a rama develop los datos de usuario

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Sistema de Registro Contable")
+
+def datos():
+    print("Ingrese los datos contables:")
+    cuenta = input("Cuenta: ")
+    descripcion = input("Descripción: ")
+    monto = float(input("Monto: "))
+    return cuenta, descripcion, monto
